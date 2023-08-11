224c71a53e99cdb5d4ee3f9f29e123200fa0afca
Dima Tsykalenko <dmitriy.tsykalenko@gmail.com>
2023-08-11 21:01:03 +0300

    added scripts

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+helm uninstall auth-api
+helm uninstall frontend
+helm uninstall log-message-processor
+helm uninstall redis
+helm uninstall todos-api
+helm uninstall users-api
+helm uninstall zipkin
