a9c496720a97540bc6e2bf63165abd998d51c783
Dima Tsykalenko <dmitriy.tsykalenko@gmail.com>
2023-08-11 21:10:13 +0300

    corrected syntax in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-helm install -f k8s/values/auth-api.yaml auth-api charts/microservices
-helm install -f k8s/values/frontend.yaml frontend charts/microservices
-helm install -f k8s/values/log-message-processor.yaml log-message-processor charts/log-message-processor
-helm install -f k8s/values/redis.yaml redis charts/redis-queue
-helm install -f k8s/values/todos-api.yaml todos-api charts/microservices
-helm install -f k8s/values/users-api.yaml users-api charts/microservices
-helm install -f k8s/values/zipkin.yaml zipkin charts/microservices
+helm install -f k8s/values/auth-api.yaml auth-api k8s/charts/microservices
+helm install -f k8s/values/frontend.yaml frontend k8s/charts/microservices
+helm install -f k8s/values/log-message-processor.yaml log-message-processor k8s/charts/log-message-processor
+helm install -f k8s/values/redis.yaml redis k8s/charts/redis-queue
+helm install -f k8s/values/todos-api.yaml todos-api k8s/charts/microservices
+helm install -f k8s/values/users-api.yaml users-api k8s/charts/microservices
+helm install -f k8s/values/zipkin.yaml zipkin k8s/charts/microservices
